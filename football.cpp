68023fcae8e3af354e73abf8020ff3c7fc0b5223
Giorgi Giorgobiani <giorgobiani504@gmail.com>
2023-08-25 15:11:43 +0400

    football

---
+++ football.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include <stdio.h>
+using namespace std;
+int i,t,x,y,b,arr;
+int main(){
+    string a;
+    cin>>a;
+    int size=a.size();
+    for(i=0;i<size;i++){
+        if(a.at(x)==a.at(x+1)){
+            b++;
+        }else b=0;
+        x++;
+        if(b>=6){
+            cout<<"YES";
+            break;
+        }
+        if((x+1)==size) break;
+    }
+    if (b<6) cout<<"NO";
+
+}
